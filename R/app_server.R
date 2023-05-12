8d6bd0e5bc94e42d1b2d30b4654db3f9b7e0dae0
Edouard <edouard.legoupil@gmail.com>
2023-05-12 11:35:33 -0500

    Adding golem mdoule

--- R/app_server.R
+++ R/app_server.R
@@ -3,23 +3,22 @@ app_server <- function(input, output, session) {
 
   wd <- setwd(getShinyOption(".appDir", getwd()))
   on.exit(setwd(wd))
-  ## Create folders to process
-  # dir.create("data-raw")
-  #dir.create("out")
+  
+  reactiveParameters <- reactiveValues() 
+  
+  #reactiveParameters <- reactiveValues(starting = TRUE)
+  #session$onFlushed(function() {
+  #  reactiveParameters$starting <- FALSE   }) 
 
-  # dir
-  # shinyDirChoose(input, 'dir',
-  #                roots = c(name=getwd()))
-  # dir <- reactive(input$dir)
-  #output$dir <- renderPrint(dir())
-
-  values <- reactiveValues(starting = TRUE)
-  session$onFlushed(function() {
-    values$starting <- FALSE   })
-  ## Declaring Variables
-  Data <- reactiveVal()
-
-
-
+ ## get the different module on server side...
+  #  mod_input_server("input_1" )
+  mod_input_server("input_1", reactiveParameters = reactiveParameters)
+  mod_categories_server("categories_1", reactiveParameters = reactiveParameters) 
+  mod_origin_server("origin_1", reactiveParameters = reactiveParameters) 
+  mod_destination_server("destination_1", reactiveParameters = reactiveParameters) 
+  mod_demographics_server("demographics_1", reactiveParameters = reactiveParameters) 
+  mod_processing_server("processing_1", reactiveParameters = reactiveParameters) 
+  mod_solution_server("solution_1", reactiveParameters = reactiveParameters) 
+  mod_migrants_server("migrants_1", reactiveParameters = reactiveParameters) 
 
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -16,68 +16,20 @@ app_ui <- function() {
     dashboardPage(
       dashboardHeader(title = "Data Literacy on Forced Displacement",
                       
-                       titleWidth = 400 #,
-                      #selectInput("hcr_region","Filter on a region",choices = sort(unique(reference$hcr_region)),multiple=F,selected="The Americas")
-                      # # Dropdown menu for messages
-                      # dropdownMenu(type = "messages", badgeStatus = "success",
-                      #              messageItem("Support Team",
-                      #                          "This is the content of a message.",
-                      #                          time = "5 mins"
-                      #              ),
-                      #              messageItem("Support Team",
-                      #                          "This is the content of another message.",
-                      #                          time = "2 hours"
-                      #              ),
-                      #              messageItem("New User",
-                      #                          "Can I get some help?",
-                      #                          time = "Today"
-                      #              )
-                      # ),
-                      # 
-                      # # Dropdown menu for notifications
-                      # dropdownMenu(type = "notifications", badgeStatus = "warning",
-                      #              notificationItem(icon = icon("users"), status = "info",
-                      #                               "5 new members joined today"
-                      #              ),
-                      #              notificationItem(icon = icon("warning"), status = "danger",
-                      #                               "Resource usage near limit."
-                      #              ),
-                      #              notificationItem(icon = icon("shopping-cart", lib = "glyphicon"),
-                      #                               status = "success", "25 sales made"
-                      #              ),
-                      #              notificationItem(icon = icon("user", lib = "glyphicon"),
-                      #                               status = "danger", "You changed your username"
-                      #              )
-                      # ),
-                      # 
-                      # # Dropdown menu for tasks, with progress bar
-                      # dropdownMenu(type = "tasks", badgeStatus = "danger",
-                      #              taskItem(value = 20, color = "aqua",
-                      #                       "Refactor code"
-                      #              ),
-                      #              taskItem(value = 40, color = "green",
-                      #                       "Design new layout"
-                      #              ),
-                      #              taskItem(value = 60, color = "yellow",
-                      #                       "Another task"
-                      #              ),
-                      #              taskItem(value = 80, color = "red",
-                      #                       "Write documentation"
-                      #              )
-                      # )
-                      
-                      
-                      ),
+                       titleWidth = 400 ),
       dashboardSidebar(
         sidebarMenu(
-          menuItem("Stories", tabName = "Stories", icon = icon("bookmark")),
-          menuItem("Categories", tabName = "Categories", icon = icon("person-walking-arrow-right")),
-          menuItem("Origin", tabName = "Origin", icon = icon("chevron-right")),
-          menuItem("Destination", tabName = "Destination", icon = icon("chevron-left")),
-          menuItem("Demographics", tabName = "Demographics", icon = icon("bars-progress")),
-          menuItem("Processing", tabName = "Processing", icon = icon("spinner")),
-          menuItem("Solution", tabName = "Solution", icon = icon("people-arrows")),
-          menuItem("Migrants", tabName = "Migrants", icon = icon("globe"))
+          menuItem(" Data Story Telling", tabName = "Stories", icon = icon("bookmark")),
+          
+          mod_input_ui("input_1"),
+          p(style = 'color:#0072BC;'," ... then explore below!"),
+          menuItem("Categories", tabName = "categories", icon = icon("person-walking-arrow-right")),
+          menuItem("Origin", tabName = "origin", icon = icon("chevron-right")),
+          menuItem("Destination", tabName = "destination", icon = icon("chevron-left")),
+          menuItem("Demographics", tabName = "demographics", icon = icon("bars-progress")),
+          menuItem("Processing", tabName = "processing", icon = icon("spinner")),
+          menuItem("Solution", tabName = "solution", icon = icon("people-arrows")),
+          menuItem("Migrants", tabName = "migrants", icon = icon("globe"))
         )
       ), ## End sidebarmenu
       dashboardBody(
@@ -87,61 +39,47 @@ app_ui <- function() {
           tabItem(tabName = "Stories",
                   #Jumbotrons are pretty, they make nice headers
                   tags$div(class = "jumbotron text-left", 
-                           style = "margin-bottom:5px;margin-top:5px;margin-left:15px",
+                           style = "margin-bottom:15px;margin-top:15px;margin-left:15px",
                            tags$h1(style = 'color:#0072BC;',
                                   style = 'margin-bottom:0px;margin-top:0px',
                                    '7 Key Questions on Forced Displacement'),
                            tags$h3(class = 'jumbotron-heading', 
                                    style = 'margin-left:25px',
-                                   ' Numbers have an important story to tell.' ),
+                                   ' Numbers have an important story to TELL' ),
                           tags$h3(class = 'jumbotron-heading', 
                                   style = 'margin-left:25px',
                                   ' They rely on YOU to give them a voice!')
                    ), ## end jumbotron..
-                    p(' 1. How the different Categories of Population of concern to UNHCR are evolving over time?'), 
-                    p('2. What are the main countries of Origin of Forced Displacement across Borders?'),  
-                    p('3. What are the main countries of Destination of Asylum of Forced Displacement across Borders?'),
-                    p('4. What are the Demographics profiles of Forcibly Displaced People in relation with the host population?'),
-                    p('5. Is the asylum Processing capacity in relation with the demand?'),
-                    p('6. What are the trends in terms of Solutions?'),
-                    p('7. What is the share of Forcibly Displaced People among total Migrants?') ,
                   
-                  
-                  p( ' ') ,
-                  p( ' ') ,
-                  p( ' ') ,
-                  p( ' ') ,
-                  p ('This app conveniently bring together a series of predefined plots in order to improve data literacy and facilitate the creation of 
-                  [persuasive data stories](https://edouard-legoupil.github.io/unhcrdatapackage/tuto/tutorial.html). 
-                  Each plot is created through a function that provides a recipe 
-                     that creates a re-usable chart in line with a [Divisible Content Strategy](https://www.columnfivemedia.com/divisible-content-strategy-gives-brand-less/).
-                     Those functions are then rendered through predefined Report templates for Countries or Regions, generated either as html, PDF or PowerPoint presentations.
-                     You may [enhance the charts story-telling ability](https://edouard-legoupil.github.io/unhcrdatapackage/articles/library.html)
-                     by adjusting the message in the title, highlighting specific parts of the data or adding annotation to ease interpretation or provide more contextual background.')
+                  br(),
+                  br(),
+                    p('  1. How the different Categories of Population of concern to UNHCR are evolving over time?'), 
+                    p('  2. What are the main countries of Origin of Forced Displacement across Borders?'),  
+                    p('  3. What are the main countries of Destination of Asylum of Forced Displacement across Borders?'),
+                    p('  4. What are the Demographics profiles of Forcibly Displaced People in relation with the host population?'),
+                    p('  5. Is the asylum Processing capacity in relation with the demand?'),
+                    p('  6. What are the trends in terms of Solutions?'),
+                    p('  7. What is the share of Forcibly Displaced People among total Migrants?') ,
+                  br(),
+                  br(),
+                  p ('This app conveniently bring together a series of predefined plots in order to improve data literacy and facilitate the creation of',
+                  tags$a(href="https://edouard-legoupil.github.io/unhcrdatapackage/tuto/tutorial.html", "persuasive data stories") ,
+                  '. Each plot is created through a function that provides a recipe that creates a re-usable chart in line with a',
+                  tags$a(href="https://www.columnfivemedia.com/divisible-content-strategy-gives-brand-less/", "Divisible Content Strategy"),
+                  '. You may',
+                  tags$a(href="https://edouard-legoupil.github.io/unhcrdatapackage/articles/library.html", "enhance the charts story-telling ability"),
+                  ' by adjusting the message in the title, highlighting specific parts of the data or adding annotation to ease interpretation or provide more contextual background.')
                   ),
           
-          tabItem(tabName = "Categories",
-                  fluidRow(   
-                    ) ),
-          tabItem(tabName = "Origin",
-                  fluidRow( 
-                  ) ),
-          tabItem(tabName = "Destination",
-                  fluidRow( 
-                    ) ),
-          tabItem(tabName = "Demographics",
-                  fluidRow(  
-                  ) ),
-          tabItem(tabName = "Processing",
-                  fluidRow( 
-                    ) ),
-          tabItem(tabName = "Solution",
-                  fluidRow( 
-                    ) ),
-          tabItem(tabName = "Migrants",
-                  fluidRow(      
-                    ) )
-
+          ## Getting the different golem modules - on the UI side.. 
+          tabItem(tabName = "categories",   mod_categories_ui("categories_1") ),
+          tabItem(tabName = "origin",       mod_origin_ui("origin_1") ),
+          tabItem(tabName = "destination",  mod_destination_ui("destination_1") ),
+          tabItem(tabName = "demographics", mod_demographics_ui("demographics_1") ),
+          tabItem(tabName = "processing",   mod_processing_ui("processing_1") ),
+          tabItem(tabName = "solution",     mod_solution_ui("solution_1") ),
+          tabItem(tabName = "migrants",     mod_migrants_ui("migrants_1") )
+          
 
           ) # End items
         ) ## dashboard Body 
